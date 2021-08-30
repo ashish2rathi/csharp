1211960c4633a706eb4c477a0cfbdb767496db38
Ashish Rathi <v-ashish@cpaperless.com>
2021-08-30 21:33:26 +0530

    Feature implementation added for "Default Interface Implementation"

---
+++ CSharp8.0/DefaultInterfaceImplementation/Logger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DefaultInterfaceImplementation
+{
+    #region Example1
+
+    interface Logger
+    {
+        void Log(string message);
+
+        void Log(Exception ex)
+        {
+            Log("Default implementation:" + ex.Message);
+        }
+    }
+
+    class CustomLogger : Logger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Implemented:" + message);
+        }
+
+        public void Log(Exception ex)
+        {
+            Log("Overridden :" + ex.Message);
+        }
+    }
+
+    #endregion Example1
+
+    #region Example2
+
+    interface Logger2
+    {
+        abstract void Log(string message);
+        virtual void Log(Exception ex)
+        {
+            Log("Default implementation:" + ex.Message);
+        }
+    }
+
+    class CustomLogger2 : Logger2
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Implemented:" + message);
+        }
+
+        public void Log(Exception message)
+        {
+            Console.WriteLine("Overridden:" + message);
+        }
+    }
+
+    #endregion Example2
+}
---
+++ CSharp8.0/DefaultInterfaceImplementation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DefaultInterfaceImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Example1
+            //var logger = new CustomLogger();
+            //Logger logger = new CustomLogger();
+            //logger.Log("Not found");
+            //logger.Log(new Exception("Not found"));
+            //Console.Read();
+            #endregion
+
+            #region Example2
+            //var logger = new Logger();
+            //Logger2 logger2 = new CustomLogger2();
+            //logger2.Log("Not found");
+            //logger2.Log(new Exception("Not found"));
+            //Console.Read();
+            #endregion
+        }
+
+
+        #region UsingStatement
+        static void CreateTextFileClassicApproach()
+        {
+            using (var file = new System.IO.StreamWriter("Sample.txt"))
+            {
+                file.WriteLine("Hello world");
+            }// file object is disposed of here
+        }
+
+        static void CreateTextFileModernApproach()
+        {
+            using var file2 = new System.IO.StreamWriter("Sample.txt");
+            file2.WriteLine("Hello world");
+            // file2 object is disposed of here
+        }
+        #endregion UsingStatement
+
+    }
+}
