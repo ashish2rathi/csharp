eed942ee55b3397e2972ba764eda0baf7d67532f
Ashish Rathi <v-ashish@cpaperless.com>
2021-11-22 21:35:27 +0530

    Few projects added, each for separate feature

---
+++ 9.0/InitOnlySetters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InitOnlySetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 9.0/ModuleInitializers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModuleInitializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
