fb6d5822a8d0a23de8110794a72c8fa50e08f9bf
Ashish Rathi <v-ashish@cpaperless.com>
2021-11-25 15:30:45 +0530

    Inheritance example added for 'Init Only Setters' feature

---
+++ 9.0/InitOnlySetters/InheritanceExample.cs
@@ -0,0 +1,24 @@
+﻿namespace InitOnlySetters
+{
+    public class Base
+    {
+        public bool Value { get; init; }
+    }
+
+    public class Derived : Base
+    {
+        public Derived()
+        {
+            // Not allowed with get only properties but allowed with init
+            Value = true;
+        }
+    }
+
+    class Consumption
+    {
+        void Example()
+        {
+            var d = new Derived() { Value = true };
+        }
+    }
+}
