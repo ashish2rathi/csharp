0cbd647b6e6ce57bbcd00352f048365d8b8ce006
Ashish Rathi <v-ashish@cpaperless.com>
2021-11-25 12:47:20 +0530

    Implemented Pattern matching feature example

---
+++ 9.0/PatternMatching/PatternCobinators.cs
@@ -0,0 +1,39 @@
+﻿namespace PatternMatching
+{
+    public class PatternCobinators
+    {
+        public static User TypeCheckingPattern(User user)
+        {
+            if (user is not null)
+            {
+                return user;
+            }
+            else
+            {
+                return new User();
+            }
+        }
+
+
+        #region RelationalPatternInIfStatement
+        //Older C#
+        public static bool IsLetterCSharp8(char c)
+        {
+            return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+        }
+
+        //C# 9
+        public static bool IsLetter(char c) => c is >= 'a' and <= 'z' or >= 'A' and <= 'Z';
+
+        //C# 9
+        public static bool IsLetterWithParanthesis(char c) => c is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z');
+
+        #endregion RelationalPatternInIfStatement
+    }
+
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
